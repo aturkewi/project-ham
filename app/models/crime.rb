3fbea634f0acd58dd3454073a96b4bfdf9c89e16
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-03 09:25:40 -0500

    Crime database partial. Need to combine multiple CSV rows into single DB row.

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: crimes
+#
+#  id                 :integer          not null, primary key
+#  borough_id         :integer
+#  murder             :integer
+#  rape               :integer
+#  robbery            :integer
+#  assault            :integer
+#  burglary           :integer
+#  grand_larceny      :integer
+#  grand_larceny_auto :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class Crime < ActiveRecord::Base
   # Bronx:1, Brooklyn:2, Manhattan:3, Queens:4, Staten_Island:5
 
@@ -6,9 +23,17 @@ class Crime < ActiveRecord::Base
   def self.load_csv_file
     csv_text = File.read('public/crime_data.csv')
     csv = CSV.parse(csv_text)
-    binding.pry
     csv.each do |row|
-      Job.create!(row.to_hash)
+      borough = Crime.find_borough_by_precinct(row[0])
+      binding.pry
     end
   end
+
+  def self.find_borough_by_precinct(precinct)
+    PRECINCT_HASH.find do|borough, precincts| 
+      precincts.include?(precinct.to_i)  
+    end.first
+  end
+
+
 end
--- test/models/crime_test.rb
+++ test/models/crime_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: crimes
+#
+#  id                 :integer          not null, primary key
+#  borough_id         :integer
+#  murder             :integer
+#  rape               :integer
+#  robbery            :integer
+#  assault            :integer
+#  burglary           :integer
+#  grand_larceny      :integer
+#  grand_larceny_auto :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 require 'test_helper'
 
 class CrimeTest < ActiveSupport::TestCase
