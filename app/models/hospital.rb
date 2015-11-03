08808d1a4ae982f48e60a2511a519ff46c54f587
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-03 11:28:50 -0500

    Update hospital to work with actual API link

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -10,17 +10,16 @@ class Hospital < ActiveRecord::Base
   BOROUGH_HASH = {"bronx"=>1, "brooklyn"=>2, "manhattan"=>3, "queens"=>4, "staten island"=>5}
 
   def self.parse_data
-    url = 'http://data.cityofnewyork.us/api/views/f7b6-v6v3/rows.json?accessType=DOWNLOAD'
+    url = 'http://data.cityofnewyork.us/resource/f7b6-v6v3.json'
     buffer = open(url).read
-    result = JSON.parse(buffer)
-    hospital_array = result["data"]
+    hospital_array = JSON.parse(buffer)
   end
 
   def self.populate_db
     hospital_array = parse_data
     hospital_array.each do |hospital_data|
-      borough_num=BOROUGH_HASH[hospital_data[9].downcase]
-      create(name:hospital_data[10], hospital_type:hospital_data[8], borough_id:borough_num)
+      borough_num=BOROUGH_HASH[hospital_data['borough'].downcase]
+      create(name:hospital_data['facility_name'], hospital_type:hospital_data['facility_type'], borough_id:borough_num)
     end
   end
 
