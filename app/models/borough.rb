50e54dd95353dd88f5c16739256d9617d7efbd48
medwards1771 <meredith.edwards1771@gmail.com>
2015-11-04 16:17:12 -0500

    modified views

--- app/models/borough.rb
+++ app/models/borough.rb
@@ -46,15 +46,4 @@ class Borough < ActiveRecord::Base
     avg_attendance=(array.inject{ |sum, x| sum + x } / array.size).round(2)
     normalizer != "nil" ? avg_attendance/self.send(normalizer) : avg_attendance
   end
-
-  # def formalize (data_set1, data_set2)
-  #   scores = {
-  #     '#{data_set1}' => Job.all.each_with_object({}) do |model, hash|
-  #       hash[model.id] = model.labor_force
-  #     end
-  #     , 
-  #     '#{data_set2}' => Hospital.all.order(:borough_id).group(:borough_id).count
-  #   }
-  #   @correlation = Pearson.coefficient(scores, 'job', 'hospital')
-  # end
 end
