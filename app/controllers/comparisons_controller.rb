e8f1421e920c2da382859d0d9e124d2dc28863db
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-30 18:09:09 -0500

    Moved some things to comparison model

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -5,30 +5,27 @@ class ComparisonsController < ApplicationController
   end
 
   def show
-   
+
     @boroughs = Borough.all
     @comparison = Comparison.new(params[:data_set_one],params[:data_set_two],params[:normalizer_one],params[:normalizer_two])
 
-    data_array_1 = @comparison.get_hash(params[:data_set_one],params[:normalizer_one]).values
-    data_array_2 = @comparison.get_hash(params[:data_set_two],params[:normalizer_two]).values
+    @comparison.set_data_arrays
+
     gon.graph_name = "#{params[:data_set_one]} vs #{params[:data_set_two]}"
-    #javascript for data1
-    gon.data1bronx = data_array_1[0]
-    gon.data1brooklyn = data_array_1[1]
-    gon.data1manhattan = data_array_1[2]
-    gon.data1queens = data_array_1[3]
-    gon.data1staten = data_array_1[4]
-    #javascript for data1
-    gon.data2bronx = data_array_2[0]
-    gon.data2brooklyn = data_array_2[1]
-    gon.data2manhattan = data_array_2[2]
-    gon.data2queens = data_array_2[3]
-    gon.data2staten = data_array_2[4]
 
+    #javascript for data1
+    gon.data1bronx = @comparison.data_array_1[0]
+    gon.data1brooklyn = @comparison.data_array_1[1]
+    gon.data1manhattan = @comparison.data_array_1[2]
+    gon.data1queens = @comparison.data_array_1[3]
+    gon.data1staten = @comparison.data_array_1[4]
 
+    #javascript for data1
+    gon.data2bronx = @comparison.data_array_2[0]
+    gon.data2brooklyn = @comparison.data_array_2[1]
+    gon.data2manhattan = @comparison.data_array_2[2]
+    gon.data2queens = @comparison.data_array_2[3]
+    gon.data2staten = @comparison.data_array_2[4]
 
   end
-
-
-
 end
--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -8,21 +8,21 @@
 #
 
 
-class Comparison 
+class Comparison
 
   attr_accessor :data1, :data2, :normalizer1, :normalizer2
-  attr_reader :correlation, :exp1, :exp2
+  attr_reader :correlation, :exp1, :exp2, :data_array_1, :data_array_2
 
   def initialize(data1, data2, normalizer1, normalizer2)
     @data1 = data1
     @data2 = data2
     @normalizer1 = normalizer1
     @normalizer2 = normalizer2
-    @exp1 = calculate_scale(get_hash(data1,normalizer1))
-    @exp2 = calculate_scale(get_hash(data2,normalizer2))
+    @exp1 = calculate_scale(get_normalized_data_by_boro(data1,normalizer1))
+    @exp2 = calculate_scale(get_normalized_data_by_boro(data2,normalizer2))
   end
 
-  def get_hash(data_set_name,normalizer)
+  def get_normalized_data_by_boro(data_set_name,normalizer)
       Borough.all.each_with_object({}) do |borough, hash|
       hash[borough.id] = borough.send(data_set_name,normalizer)
     end
@@ -35,8 +35,8 @@ class Comparison
   end
 
   def calculate_correlation
-    data_hash_1 = get_hash(data1,normalizer1)
-    data_hash_2 = get_hash(data2,normalizer2)
+    data_hash_1 = get_normalized_data_by_boro(data1,normalizer1)
+    data_hash_2 = get_normalized_data_by_boro(data2,normalizer2)
 
     scores = {
       first_data:data_hash_1,  # => {1=>14, 2=>26, 3=>24, 4=>11, 5=>3}
@@ -44,5 +44,11 @@ class Comparison
     }
     @correlation = Pearson.coefficient(scores, :first_data, :second_data)
   end
- 
+
+  def set_data_arrays
+    @data_array_1 = get_normalized_data_by_boro(data1, normalizer1).values
+    @data_array_2 = get_normalized_data_by_boro(data2, normalizer2).values
+  end
+
+
 end
